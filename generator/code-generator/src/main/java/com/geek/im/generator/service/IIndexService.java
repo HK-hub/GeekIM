48137933b5e7bfa9fe34fc5baa2b9d8b6a2467d5
HK-hub <3161880795@qq.com>
2023-11-05 21:08:05 +0800

    feat: 添加第三方代码生成器实现

---
+++ generator/code-generator/src/main/java/com/geek/im/generator/service/IIndexService.java
@@ -0,0 +1,24 @@
+package com.geek.im.generator.service;
+
+import com.fengwenyi.api.result.ResponseTemplate;
+import com.fengwenyi.codegenerator.vo.CodeGeneratorRequestVo;
+
+/**
+ * @author <a href="https://www.fengwenyi.com">Erwin Feng</a>
+ * @since 2021-07-12
+ */
+public interface IIndexService {
+
+    /**
+     * 生成代码
+     * @param requestVo
+     * @return
+     */
+    ResponseTemplate<Void> codeGenerator(CodeGeneratorRequestVo requestVo);
+
+    /**
+     * 升级检查
+     * */
+    String upgrade(String version);
+
+}
